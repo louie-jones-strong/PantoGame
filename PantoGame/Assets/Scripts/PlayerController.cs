c5c59ca126aa9019928e44d0b2ea9adaeab059e4
louie jones-strong Laptop <louiej-s@hotmail.co.uk>
2020-10-03 23:26:06 +0100

    basic walking test setup

---
+++ PantoGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+    public float Drag;
+    Vector3 Velocity;
+    
+
+
+    void Update()
+    {
+        var acceleration = new Vector3();
+        acceleration.x = -(Velocity.x * Drag);
+        acceleration.y = 0;
+
+        var recentDpad = SimpleInput.GetRecentDpad();
+        if (SimpleInput.GetInputActive(recentDpad))
+        {
+            switch (recentDpad)
+            {
+                case EInput.dpadLeft:
+                {
+                    acceleration.x -= Speed;
+                    break;
+                }
+                case EInput.dpadRight:
+                {
+                    acceleration.x += Speed;
+                    break;
+                }
+                case EInput.dpadUp:
+                {
+                    break;
+                }
+                default:
+                {
+                    break;
+                }
+            }
+        }
+
+        acceleration *= Time.deltaTime;
+        Velocity += acceleration;
+        transform.position += (Velocity * Time.deltaTime);
+    }
+}
