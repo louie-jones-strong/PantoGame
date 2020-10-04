2f772f231a3ce8d197283dce75104dbfbec45676
Louie Jones-Strong <louie.jonesstrong@t-tales.com>
2020-10-04 21:42:15 +0100

    dash hook up

--- PantoGame/Assets/Scripts/Player/PlayerController.cs
+++ PantoGame/Assets/Scripts/Player/PlayerController.cs
@@ -7,8 +7,14 @@ public class PlayerController : MonoBehaviour
 {
     public PhysicsRotation[] PhysicsParts;
     public float Speed;
+	public float DashSpeedMultiplier;
+	public float DashCoolDown = 1.5f;
+	public float DashAffectTime = 0.5f;
     public float Drag;
     Vector3 Velocity;
+
+	float TimeLeftOfDash;
+	float DashCoolDownLeft;
     
     void Start()
     {
@@ -27,6 +33,22 @@ public class PlayerController : MonoBehaviour
         {
             acceleration.x += Speed;
         }
+		if (SimpleInput.GetInputState(EInput.A) == EButtonState.Pressed &&
+			DashCoolDownLeft <= 0)
+		{
+			TimeLeftOfDash = DashAffectTime;
+			DashCoolDownLeft = DashCoolDown;
+		}
+
+		if (TimeLeftOfDash > 0)
+		{
+			acceleration *= DashSpeedMultiplier;
+			TimeLeftOfDash -= Time.deltaTime;
+		}
+		if (DashCoolDownLeft > 0)
+		{
+			DashCoolDownLeft -= Time.deltaTime;
+		}
 
         acceleration.x += -(Velocity.x * Drag);
 
