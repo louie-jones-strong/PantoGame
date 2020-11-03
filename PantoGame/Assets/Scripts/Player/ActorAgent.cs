1d7995f08fc47af68755bfe48643bd69e1734a0a
Louie Jones-Strong <louie.jonesstrong@t-tales.com>
2020-11-03 00:34:54 +0000

    add actor to the game

---
+++ PantoGame/Assets/Scripts/Player/ActorAgent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ActorAgent : Agent
+{
+	ActorTask CurrentTask;
+	
+	public void Setup()
+	{
+		CameraController.AddTarget(transform, weighting:Settings.ActorCamWeighting);
+	}
+
+	void Update()
+    {
+		UpdateVisuals();
+    }
+}
--- PantoGame/Assets/Scripts/Settings.cs
+++ PantoGame/Assets/Scripts/Settings.cs
@@ -4,6 +4,7 @@ public class Settings
 	public const float RotateToCamMultiplier = 0.5f;
 	public const float CamWeightingCutOff = 2f;
 	public const float AudienceCamWeighting = 1f;
+	public const float ActorCamWeighting = 50f;
 	public const float PlayerCamWeighting = 100f;
 	public const float DeadZone = 0.1f;
 
