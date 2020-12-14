6e449732e323a5ac646ad147bdd6f8d8031ba01b
Louie Jones-Strong <louie.jonesstrong@t-tales.com>
2020-12-14 23:01:33 +0000

    show tasks in task list ui

--- PantoGame/Assets/Scripts/Hud/TaskItemUi.cs
+++ PantoGame/Assets/Scripts/Hud/TaskItemUi.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TaskItemUi : MonoBehaviour
 {
-	
+	[SerializeField] Text SceneName;
+
+	public void Setup(Task task)
+	{
+		gameObject.SetActive(true);
+		SceneName.text = task.TaskId;
+	}
 }
--- PantoGame/Assets/Scripts/Hud/TaskListUi.cs
+++ PantoGame/Assets/Scripts/Hud/TaskListUi.cs
@@ -7,7 +7,14 @@ public class TaskListUi : MonoBehaviour
 {
 	[SerializeField] TaskItemUi Prefab;
 	[SerializeField] Text SceneName;
+
+	List<TaskItemUi> TaskItemPool = new List<TaskItemUi>();
 	
+	void Awake()
+	{
+		Prefab.gameObject.SetActive(false);
+	}
+
 	void Update()
 	{
 		if (Theatre.Instance?.CurrentScript.CurrentScene == null)
@@ -19,9 +26,36 @@ public class TaskListUi : MonoBehaviour
 
 		SceneName.text = $"Scene: {scene.name}";
 
+		HideAllPoolItems();
+
 		foreach (var task in scene.Tasks)
 		{
-			
+			var item = GetItemUi();
+			item.Setup(task);
 		}
+	}
+
+	void HideAllPoolItems()
+	{
+		foreach (var item in TaskItemPool)
+		{
+			item.gameObject.SetActive(false);
+		}
+	}
+
+	TaskItemUi GetItemUi()
+	{
+		foreach (var item in TaskItemPool)
+		{
+			if (!item.gameObject.activeSelf)
+			{
+				return item;
+			}
+		}
+		var instance = Instantiate<TaskItemUi>(Prefab, transform);
+
+		TaskItemPool.Add(instance);
+		
+		return instance;
 	}
 }
